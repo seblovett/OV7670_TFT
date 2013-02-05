562eecff5c7641a5f8495b3ed8ecff5d1aeb6d21
Henry Lovett <hl13g10@soton.ac.uk>
2013-02-05 21:09:39 +0000

    Initial Commit

---
+++ Camera_TFT/Camera_TFT.cpp
@@ -0,0 +1,17 @@
+/*
+ * Camera_TFT.cpp
+ *
+ * Created: 05/02/2013 21:08:58
+ *  Author: hslovett
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
